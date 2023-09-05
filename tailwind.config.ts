a19da260e196dfd779ef7fd8734d8760a421a1d5
Thomas <totoalexandre6259@gmail.com>
2023-09-05 14:48:31 +0200

    styles config

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -8,11 +8,56 @@ const config: Config = {
   ],
   theme: {
     extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+      fontFamily: {
+        'spartan': ['League Spartan', 'sans-serif'],
+        'Lora': ['Lora', 'serif'],
+        'Poppins': ['Poppins', 'sans-serif'],
+        'Roboto': ['Roboto', 'sans-serif'],
       },
+      colors: {
+        'active': '#D4A373',
+        'primary': '#E5E5E5',
+        'secondary': '#6C757D',
+        'icon': '#F8F9FA',
+        'social-icons': '#E5E5E5',
+        'white' : '#FFF',
+        'title': '#495057'
+      },
+      backgroundColor: {
+        'header-color-slider': 'rgba(0, 0, 0, 0.2)',
+        'header': 'rgba(0, 0, 0, 1)',
+        'category': 'rgba(255, 255, 255, 0.15)',
+        'footer' : '#212529'
+      },
+      maxWidth: {
+        '10xl': '90rem',
+      },
+      height: {
+        '4.75xl' : '37.5rem',
+        'card':'35.313rem'
+      },
+      width: {
+        '4.33xl':'33.125rem',
+        'card':'19.375rem',
+      },
+      zIndex: {
+        '100': '100',
+        '99': '99',
+        '98': '98',
+        '97': '97',
+      },
+      backgroundImage: {
+        'nav-responsive': 'url("/assets/images/homeBg1.png")',
+        'skew-responsive': 'url("/assets/images/homeBg2.png")',
+        'header-image': 'url("/assets/images/homeBg1.png")',
+        'bigPost' : 'url("/assets/images/bigPost.png")',
+      },
+      borderColor: {
+        'nav': '#D4A373'
+      },
+      margin: {
+        '17.5': '70px',
+      }     
     },
   },
   plugins: [],
