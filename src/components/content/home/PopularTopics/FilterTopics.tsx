b00a172cece050176fd5a2e7bc84b6cc239dd762
Thomas <totoalexandre6259@gmail.com>
2023-09-06 08:30:56 +0200

    Need database link

---
+++ src/components/content/home/PopularTopics/FilterTopics.tsx
@@ -0,0 +1,28 @@
+import React from "react";
+import CardTopics from "@/components/card/cardTopics/CardTopics";
+import { useActualCategory } from "../../../context/ActualCategory";
+const FilterTopics = () => {
+  const { filterCategory, setFilterCategory } = useActualCategory();
+  console.log(filterCategory);
+
+  const topics = [
+    { category: "all" },
+    { category: "Adventure" },
+    { category: "Travel" },
+    { category: "Fashion" },
+  ];
+  return (
+    <>
+      {topics
+        .filter(
+          (topic) =>
+            topic.category === filterCategory || filterCategory === "all"
+        )
+        .map((topic, index) => (
+          <CardTopics key={index} category={topic.category} />
+        ))}
+    </>
+  );
+};
+
+export default FilterTopics;
