f8f9168e85fc6443ec23190c78d380c75786e759
Thomas <totoalexandre6259@gmail.com>
2023-09-06 15:38:12 +0200

    Remove ts error ad ServComponent ServBigPost

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,16 +1,15 @@
-
 import Template from "@/components/template/Template";
 import HomeSliders from "@/components/slider/HomeSliders";
 import PopularTopics from "@/components/content/home/PopularTopics/PopularTopics";
-import BigPost from "@/components/content/home/BigPost/BigPost";
 import EditorPick from "@/components/content/home/EditorPick/EditorPick";
-
+import ServBigPost from "@/components/content/home/BigPost/ServBigPost";
 export default function Home() {
   return (
     <Template sliders={<HomeSliders />}>
       {/* @ts-expect-error Server Component */}
       <PopularTopics />
-      <BigPost />
+      {/* @ts-expect-error Server Component */}
+      <ServBigPost />
       <EditorPick />
     </Template>
   );
