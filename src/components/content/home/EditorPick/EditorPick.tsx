b6ef3d4dc3f86ab23995e2e881e9c48b9fc7cb43
Thomas <totoalexandre6259@gmail.com>
2023-09-06 16:37:54 +0200

    Dynamique Editor Pick OK

--- src/components/content/home/EditorPick/EditorPick.tsx
+++ src/components/content/home/EditorPick/EditorPick.tsx
@@ -3,31 +3,43 @@ import React from "react";
 import H1Title from "@/components/utilities/H1Title";
 import CardEditorPick from "@/components/card/cardEditorPick/CardEditorPick";
 
-const EditorPick = () => {
+interface Post {
+  id: number;
+  title: string;
+  description: string;
+  image: string;
+  category: { id: number; createdAt: Date; name: string } | null;
+  createdAt: Date;
+}
+
+interface BigPostProps {
+  posts: Post[];
+}
+
+const EditorPick = (posts: BigPostProps) => {
+  const postEditorPick = posts.posts;
+  console.log(posts);
+
   return (
     <div className="xl:h-4.75xl h-auto w-full xl:py-20 py-10 max-w-10xl mx-auto">
       <div className="flex flex-col w-full h-full">
-        <H1Title className="text-title xl:pb-4 xl:py-0 py-4 xl:px-0 px-2" title="Editor's Pick" />
+        <H1Title
+          className="text-title xl:pb-4 xl:py-0 py-4 xl:px-0 px-2"
+          title="Editor's Pick"
+        />
         <div className="flex xl:flex-row flex-col xl:px-0 px-2 h-full w-full gap-4">
-          <CardEditorPick
-            image={"EditorPick1.png"}
-            date={"08.08.2021"}
-            title={"Richird Norton photorealistic rendering as real photos"}
-            description={
-              "Progressively incentivize cooperative systems through technically sound functionalities. The credibly productivate seamless data."
-            }
-            category={["Fashion", "Travel"]}
-          />
-
-          <CardEditorPick
-            image={"EditorPick2.png"}
-            date={"08.08.2021"}
-            title={"Richird Norton photorealistic rendering as real photos"}
-            description={
-              "Progressively incentivize cooperative systems through technically sound functionalities. The credibly productivate seamless data."
-            }
-            category={["Fashion", "Travel"]}
-          />
+          {postEditorPick.map(
+            ({ title, description, image, category, createdAt, id }) => (
+              <CardEditorPick
+                key={id}
+                image={image}
+                date={createdAt.toISOString().split("T")[0]}
+                title={title}
+                description={description}
+                category={category ? category.name : "N/A"}
+              />
+            )
+          )}
         </div>
       </div>
     </div>
---
+++ src/components/content/home/EditorPick/ServEditorPick.tsx
@@ -0,0 +1,27 @@
+"use server";
+import prisma from "@/db/prisma";
+import EditorPick from "./EditorPick";
+
+const ServEditorPick = async () => {
+  const posts = await prisma.post.findMany({
+    where: {
+      published: true,
+      editorPick: true,
+    },
+    select: {
+      id: true,
+      title: true,
+      description: true,
+      image: true,
+      category: true,
+      createdAt: true,
+    },
+  });
+
+  console.log(posts);
+  if (posts.length > 1) {
+    return <EditorPick posts={posts} />;
+  }
+};
+
+export default ServEditorPick;
