5345bade0c44047d0bf01f258b03c7d579ac533c
Thomas <totoalexandre6259@gmail.com>
2023-09-06 15:38:39 +0200

    dynamique component with table post

--- src/components/content/home/BigPost/BigPost.tsx
+++ src/components/content/home/BigPost/BigPost.tsx
@@ -1,21 +1,43 @@
-import React from "react";
+"use server";
+
 import CategoryUi from "@/components/utilities/CategoryUi";
 import H1Title from "@/components/utilities/H1Title";
 import StandardParagraphe from "@/components/utilities/StandardParagraphe";
 import HorizontalSeparator from "@/components/utilities/HorizontalSeparator";
 import { Date } from "@/components/utilities/Date";
-const BigPost = () => {
+
+interface Post {
+  published: boolean;
+  bigPost: boolean;
+  title: string;
+  description: string;
+  image: string;
+  category: { id: number; createdAt: Date; name: string } | null;
+  createdAt: Date;
+}
+
+interface BigPostProps {
+  post: Post;
+}
+
+const BigPost = (post: BigPostProps) => {
+  const { title, description, image, category, createdAt } = post.post;
+  const nameCategory = category?.name;
+  const formattedDate = createdAt.toISOString().split("T")[0];
+
   return (
-    <div className="h-4.75xl w-full bg-bigPost bg-cover bg-no-repeat bg-top">
+    <div
+      className="h-4.75xl w-full  bg-cover bg-no-repeat bg-top"
+      style={{
+        backgroundImage: `url(/assets/images/${image})`,
+      }}
+    >
       <div className="flex flex-col items-center text-center gap-5 h-full justify-center w-1/3 mx-auto">
-        <CategoryUi category="Fashion" />
-        <H1Title
-          className="text-white"
-          title="Richird Norton photorealistic rendering as real photos"
-        />
-        <StandardParagraphe content="Progressively incentivize cooperative systems through technically sound functionalities. The credibly productivate seamless data." />
+        {nameCategory && <CategoryUi category={nameCategory} />}
+        <H1Title className="text-white" title={title} />
+        <StandardParagraphe content={description} />
         <HorizontalSeparator />
-        <Date date="08.08.2021" />
+        <Date date={formattedDate} />
       </div>
     </div>
   );
