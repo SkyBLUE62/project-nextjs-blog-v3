7cdf0b4a603de446e6ebbb8d5cf7e557a7cd75c5
Thomas <totoalexandre6259@gmail.com>
2023-09-08 22:28:36 +0200

    filter and map card table post

--- src/components/content/home/PopularTopics/FilterTopics.tsx
+++ src/components/content/home/PopularTopics/FilterTopics.tsx
@@ -1,24 +1,34 @@
-import React from "react";
 import CardTopics from "@/components/card/cardTopics/CardTopics";
 import { useActualCategory } from "../../../context/ActualCategory";
-const FilterTopics = () => {
-  const { filterCategory, setFilterCategory } = useActualCategory();
 
-  const topics = [
-    { category: "all" },
-    { category: "Adventure" },
-    { category: "Travel" },
-    { category: "Fashion" },
-  ];
+interface Post {
+  id: number;
+  title: string;
+  description: string;
+  image: string;
+  category: { id: number; createdAt: Date; name: string } | null;
+  createdAt: Date;
+  user: { id: number; name: string; job: string };
+}
+
+type Props = {
+  popularPosts: Post[];
+};
+
+const FilterTopics = (popularPosts: Props) => {
+  const { filterCategory, setFilterCategory } = useActualCategory();
+  const posts = popularPosts.popularPosts;
+
   return (
     <>
-      {topics
+      {posts
         .filter(
-          (topic) =>
-            topic.category === filterCategory || filterCategory === "all"
+          (post) =>
+            (post.category && post.category.name === filterCategory) ||
+            filterCategory === "all"
         )
-        .map((topic, index) => (
-          <CardTopics key={index} category={topic.category} />
+        .map((post, index) => (
+          <CardTopics key={index} post={post} />
         ))}
     </>
   );
