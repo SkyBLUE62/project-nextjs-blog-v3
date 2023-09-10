2fb3a74810705f5bd3bb5f1857eae0b91881ab5c
Thomas <totoalexandre6259@gmail.com>
2023-09-10 19:47:27 +0200

    recup slide in db and pass data to homeSliders component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,30 @@
+"use server";
+
 import Template from "@/components/template/Template";
 import HomeSliders from "@/components/slider/HomeSliders";
 import PopularTopics from "@/components/content/home/PopularTopics/PopularTopics";
 import ServBigPost from "@/components/content/home/BigPost/ServBigPost";
 import ServEditorPick from "@/components/content/home/EditorPick/ServEditorPick";
+import prisma from "@/db/prisma";
 
-export default function Home() {
+export default async function Home() {
+  const slides = await prisma.post.findMany({
+    where: {
+      published: true,
+      slide: true,
+    },
+    select: {
+      id: true,
+      title: true,
+      description: true,
+      image: true,
+      createdAt: true,
+      category: true,
+    },
+  });
+
   return (
-    <Template sliders={<HomeSliders />}>
+    <Template sliders={slides && <HomeSliders slides={slides} />}>
       {/* @ts-expect-error Server Component */}
       <PopularTopics />
       {/* @ts-expect-error Server Component */}
