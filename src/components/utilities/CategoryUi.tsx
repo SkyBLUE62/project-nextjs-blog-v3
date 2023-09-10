3a657f36bd0b98f00690f7e91d193093f3370614
Thomas <totoalexandre6259@gmail.com>
2023-09-10 19:50:09 +0200

    add props classname

--- src/components/utilities/CategoryUi.tsx
+++ src/components/utilities/CategoryUi.tsx
@@ -2,11 +2,14 @@ import React from "react";
 
 interface CategoryProps {
   category: string;
+  className?: string;
 }
 
-const Category: React.FC<CategoryProps> = ({ category }) => {
+const Category: React.FC<CategoryProps> = ({ category, className }) => {
   return (
-    <div className="bg-category py-2 px-2 rounded-lg uppercase text-white font-Roboto text-center font-bold text-xs">
+    <div
+      className={`bg-category py-2 px-2 rounded-lg uppercase text-white font-Roboto text-center font-bold text-xs ${className} `}
+    >
       {category}
     </div>
   );
