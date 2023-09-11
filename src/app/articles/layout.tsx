3f1a4dfc365a29184666a69711bcc7a39da143dd
Thomas <totoalexandre6259@gmail.com>
2023-09-11 19:16:31 +0200

    Layout articles

---
+++ src/app/articles/layout.tsx
@@ -0,0 +1,35 @@
+import type { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "RUNO | All Articles",
+  description: "All articles published by RUNO",
+};
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <html lang="en">
+      <head>
+        <link
+          href="https://fonts.googleapis.com/css2?family=Inter:wght@300;400;600;700;800&family=League+Spartan:wght@300;400;500;600;700;800;900&family=Lora:wght@400;500;600;700&family=Poppins:wght@100;300;400;500;600;800&display=swap"
+          rel="stylesheet"
+        />
+
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link
+          rel="preconnect"
+          href="https://fonts.gstatic.com"
+          crossOrigin="anonymous"
+        />
+        <link
+          href="https://fonts.googleapis.com/css2?family=Inter:wght@300;400;600;700;800&family=League+Spartan:wght@300;400;500;600;700;800;900&family=Lora:wght@400;500;600;700&family=Poppins:wght@100;300;400;500;600;800&family=Roboto:wght@100;300;400;500;700;900&display=swap"
+          rel="stylesheet"
+        />
+      </head>
+      <body>{children}</body>
+    </html>
+  );
+}
