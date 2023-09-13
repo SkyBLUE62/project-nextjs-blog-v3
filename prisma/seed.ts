81ff888118bc064666b62d13983dc4331a746f37
Thomas <totoalexandre6259@gmail.com>
2023-09-13 21:27:28 +0200

    seed update

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -2,19 +2,43 @@ import { PrismaClient } from '@prisma/client'
 const prisma = new PrismaClient()
 async function main() {
 
-        const post = await prisma.post.create({
-            data:{
-                title:"Richird Norton photorealistic rendering as real photos",
-                description:"Progressively incentivize cooperative systems through technically sound functionalities. The credibly productivate seamless data.",
-                image: "bigPost.png",
-                published:true,
-                userId:1,
-                categoryId:1,
-                editorPick:true,
-                bigPost:false,
-                popularTopics:true
-            }
-        })
+  const post = await prisma.post.create({
+    data:{
+        title:"Richird Norton photorealistic rendering as real photos",
+        description:"Progressively incentivize cooperative systems through technically sound functionalities. The credibly productivate seamless data.",
+        image: "bigPost.png",
+        published:true,
+        userId:"clmhwjxhw0000v51o7usq6u96",
+        categoryId:4,
+        editorPick:false,
+        bigPost:false,
+        popularTopics:true,
+        slide:true
+    }
+})
+
+
+// const category = await prisma.category.createMany({
+// data:[
+// {
+//   name: "adventure",
+// },
+// {
+//   name: "fashion",
+// },
+// {
+//   name: "technologies",
+// },
+// {
+//   name: "travel",
+// },
+// {
+//   name: "Branding",
+// },
+
+// ]
+// })
+  
 
 }
 main()
