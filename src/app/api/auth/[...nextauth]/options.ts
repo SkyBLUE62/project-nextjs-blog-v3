1406253df11710e271a67b207ab029575d420961
Thomas <totoalexandre6259@gmail.com>
2023-09-14 19:07:44 +0200

    Auth-next Options

---
+++ src/app/api/auth/[...nextauth]/options.ts
@@ -0,0 +1,11 @@
+import type { NextAuthOptions } from "next-auth";
+
+export const options: NextAuthOptions = {
+    providers: [
+        
+    ],
+    pages: {
+        signIn: "/signin",
+        newUser: "/signup",
+    }
+}
