5563b8cfb43a37246f3c78986fcb65decd6857ae
Thomas <totoalexandre6259@gmail.com>
2023-09-19 20:17:30 +0200

    Next auth Credentials options

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -1,11 +1,36 @@
-import NextAuth from "next-auth";
+import  CredentialsProvider  from 'next-auth/providers/credentials';
+import NextAuth, {NextAuthOptions} from "next-auth";
 import { PrismaAdapter } from "@auth/prisma-adapter";
 import { PrismaClient } from "@prisma/client";
-import AppleProvider from "next-auth/providers/apple"
-
+import bcrypt from "bcrypt";
 
 const prisma = new PrismaClient();
 
-const handler = NextAuth()
+const authOptions: NextAuthOptions = {
+    adapter: PrismaAdapter(prisma),
 
-export {handler as GET, handler as POST}
+    providers: [
+        CredentialsProvider({
+            type: 'credentials',
+            credentials: {},
+            async authorize(credentials, req) {
+                const {email, password} = credentials as {email: string, password: string};
+
+                if (await prisma.user.findFirst({where: {email}})) {
+                    const user = await prisma.user.findFirst({where: {email}});
+                    if ( user && await bcrypt.compare(password, user.password)) {
+                        return user;
+                    } else {
+                        return null; 
+                    }
+                
+                }
+                return null;
+            }
+        })
+    ],
+
+}
+
+export default NextAuth(authOptions)
+
