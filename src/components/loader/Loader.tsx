4a94e630be564f4988a6d24bde322d3537805acb
Thomas <totoalexandre6259@gmail.com>
2023-09-22 21:19:14 +0200

    Loader style

--- src/components/loader/Loader.tsx
+++ src/components/loader/Loader.tsx
@@ -1,9 +1,38 @@
-"use client"
-import LoaderSVG from "../../../public/assets/images/Loader.svg";
 export const Loader = () => {
   return (
-    <div>
-      <LoaderSVG />
+    <div className="flex justify-center items-center">
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        style={{
+          margin: "auto",
+          background: "rgb(255, 255, 255)",
+          display: "block",
+          shapeRendering: "auto",
+        }}
+        width="200px"
+        height="200px"
+        viewBox="0 0 100 100"
+        preserveAspectRatio="xMidYMid"
+      >
+        <circle
+          cx="50"
+          cy="50"
+          fill="none"
+          stroke="#212529"
+          strokeWidth="10"
+          r="35"
+          strokeDasharray="164.93361431346415 56.97787143782138"
+        >
+          <animateTransform
+            attributeName="transform"
+            type="rotate"
+            repeatCount="indefinite"
+            dur="1s"
+            values="0 50 50;360 50 50"
+            keyTimes="0;1"
+          ></animateTransform>
+        </circle>
+      </svg>
     </div>
   );
 };
