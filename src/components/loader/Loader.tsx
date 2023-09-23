d4565ae80682dbf4472f6a7ad6d5de8643d5fcc2
Thomas <totoalexandre6259@gmail.com>
2023-09-23 20:43:45 +0200

    center loader

--- src/components/loader/Loader.tsx
+++ src/components/loader/Loader.tsx
@@ -1,6 +1,6 @@
 export const Loader = () => {
   return (
-    <div className="flex justify-center items-center">
+    <div className="flex justify-center items-center h-screen w-screen">
       <svg
         xmlns="http://www.w3.org/2000/svg"
         style={{
