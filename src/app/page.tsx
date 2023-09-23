4af789b5c95016f42b85cb64a92060fd41de0d2d
Thomas <totoalexandre6259@gmail.com>
2023-09-23 20:43:01 +0200

    remove Suspense import

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,8 +6,7 @@ import PopularTopics from "@/components/content/home/PopularTopics/PopularTopics
 import ServBigPost from "@/components/content/home/BigPost/ServBigPost";
 import ServEditorPick from "@/components/content/home/EditorPick/ServEditorPick";
 import prisma from "@/db/prisma";
-import { Suspense } from "react";
-import { Loader } from "@/components/loader/Loader";
+
 
 export default async function Home() {
   const slides = await prisma.post.findMany({
