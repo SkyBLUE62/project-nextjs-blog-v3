8be1b0f65bac9118b8d845330fc73aae37e022ca
Thomas <totoalexandre6259@gmail.com>
2023-09-27 19:34:26 +0200

    request category

--- src/app/add-article/page.tsx
+++ src/app/add-article/page.tsx
@@ -1,14 +1,27 @@
-"use server"
+"use server";
 import Template from "@/components/template/Template";
 import AddArticleForm from "@/components/form/AddArticleForm";
+import prisma from "@/db/prisma";
 
-const addArticle = () => {
+type Category = {
+  id: number;
+  name: string;
+}[];
+
+const addArticle = async () => {
+  const category: Category = await prisma.category.findMany({
+    select: {
+      id: true,
+      name: true,
+    },
+  });
+
   return (
     <Template>
       <div className="h-1/2 w-full mx-auto">
         <div className="flex flex-col items-center justify-center">
           <h1>Add Article</h1>
-          <AddArticleForm />
+          <AddArticleForm category={category} />
         </div>
       </div>
     </Template>
