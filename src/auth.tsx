7a91e7154ab0bb000acc1f208818dd4dccec0f47
Thomas <totoalexandre6259@gmail.com>
2023-09-28 20:38:44 +0200

    add isAdmin

--- src/auth.tsx
+++ src/auth.tsx
@@ -6,4 +6,13 @@ const isLogin = () => {
   return session;
 };
 
+const isAdmin = () => {
+  const session = getServerSession(authOptions);
+  // if (session && session.user.role === "ADMIN") {
+  //   return true;
+  // } else {
+  //   return false;
+  // }
+};
+
 export { isLogin };
