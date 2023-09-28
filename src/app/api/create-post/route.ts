8d3bb9c849a57b97566f929e1aeec2148b0eb720
Thomas <totoalexandre6259@gmail.com>
2023-09-28 20:39:22 +0200

    create post

--- src/app/api/create-post/route.ts
+++ src/app/api/create-post/route.ts
@@ -2,8 +2,15 @@ import prisma from "@/db/prisma";
 import { NextRequest, NextResponse } from "next/server";
 import { join } from "path";
 import fs from 'fs';
+import { getServerSession } from "next-auth";
 
 export async function POST(req: NextRequest, res: NextResponse) {
+
+  const session = await getServerSession();
+  if (!session) {
+    return NextResponse.json({ success: false });
+  }else{
+  console.log(session);
     const data = await req.formData();
     const image = data.get("image") as File | null;
   
@@ -14,25 +21,48 @@ export async function POST(req: NextRequest, res: NextResponse) {
     try {
       const bytes = await image.arrayBuffer();
       const buffer = Buffer.from(bytes);
-  
-      const fileName = image.name;
-  
+      const extension = image.name.split(".")[1];
+
+      const newFileName = `${Date.now()}.${extension}`;
       // Spécifiez le chemin complet du fichier de destination dans le dossier "public"
-      const pathToFile = join(process.cwd(), "public", "uploads", fileName);
+      const pathToFile = join(process.cwd(), "public", "assets", "images", "uploads", newFileName);
   
-      // Vérifiez si le répertoire "public/uploads" existe, sinon créez-le
-      if (!fs.existsSync(join(process.cwd(), "public", "uploads"))) {
-        fs.mkdirSync(join(process.cwd(), "public", "uploads"), { recursive: true });
+      // Vérifiez si le répertoire "public/assets/images/uploads" existe, sinon créez-le
+      if (!fs.existsSync(join(process.cwd(), "public", "assets", "images", "uploads"))) {
+        fs.mkdirSync(join(process.cwd(), "public", "assets", "images", "uploads"), { recursive: true });
       }
   
-      // Écrivez le fichier dans le dossier "public/uploads"
+      // Écrivez le fichier dans le dossier "public/assets/images/uploads"
       fs.writeFileSync(pathToFile, buffer);
   
-      console.log(`Fichier enregistré dans le dossier public : /uploads/${fileName}`);
-      return NextResponse.json({ success: true, path: `/uploads/${fileName}` });
+      console.log(`Fichier enregistré dans le dossier public : assets/images/uploads/${newFileName}`);
+      console.log(data.get("category"));
+      if (session && session.user && session.user.email) {
+        await prisma.post.create({
+          data: {
+            title: data.get("title") as string,
+            description: data.get("description") as string,
+            content: data.get("content") as string,
+            image: `/assets/images/uploads/${newFileName}`,
+            user: {
+              connect: {
+                email: session.user.email as string,
+              },
+            },
+            category: {
+              connect: {
+                id: Number(data.get("category")) as unknown as number,
+              },
+            },
+          },
+        });
+      }else{
+        return NextResponse.json({ success: false });
+      }
+      return NextResponse.json({ success: true, path: `/assets/images/uploads/${newFileName}` });
     } catch (error: any) {
       console.error('Erreur lors du traitement de l\'image :', error);
       return NextResponse.json({ success: false, error: error.message });
     }
-
+  }
 }
