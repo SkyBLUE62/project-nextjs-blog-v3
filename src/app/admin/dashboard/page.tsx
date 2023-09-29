57a38a7034b8d667985b92e4eed02b1a99c54012
Thomas <totoalexandre6259@gmail.com>
2023-09-29 13:40:35 +0200

    middleware isAdmin

--- src/app/admin/dashboard/page.tsx
+++ src/app/admin/dashboard/page.tsx
@@ -1,10 +1,25 @@
 "use server";
 
+import { authOptions } from "@/app/api/auth/[...nextauth]/route";
 import { getServerSession } from "next-auth";
+import { redirect } from "next/navigation";
+
+type User = {
+  id: string;
+  name: string;
+  email: string;
+  image: string;
+  role: "ADMIN" | "USER";
+  randomKey: string;
+};
 
 const dashboard = async () => {
-  const session = await getServerSession();
-  console.log(session);
+  const session: { user: User } | null = await getServerSession(authOptions);
+  console.log(session?.user);
+  if (!session || session.user.role !== "ADMIN") {
+    redirect("/");
+  }
+
   return <h1>dashboard Admin</h1>;
 };
 
